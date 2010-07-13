f3115f354865079b2effd765e0ab07357a471f76
sevas <none@none>
2010-07-13 20:26:34 +0200

    cleaned up the mpl demo

--- src/demo_matplotlib.py
+++ src/demo_matplotlib.py
@@ -12,25 +12,21 @@ import noise
 
 
 if __name__ == '__main__':
-    w, h = 256,256
-    n = 8
-    fractal_sum = np.zeros((w, h))
-    turbulence = np.zeros_like(fractal_sum)
-    ridges = np.zeros_like(fractal_sum)
-    noise_values = np.zeros_like(fractal_sum)
+    w, h = 512,512
+    n = 5
+    texture = np.zeros((w, h))
 
     for i in range(w-1):
         for j in range(h-1):
             x, y = float(i)/(w), float(j)/(h)
-            #fractal_sum[i, j] = noise.make_fractal_sum_2D(x, y, n)
-            #turbulence[i, j] = noise.make_turbulence_2D(x, y, n)
-            #noise_values[i, j] = noise.make_ridgedmf(x, y, n, 5.0)
-            noise_values[i, j] = noise.make_marble_2D(x, y, n)
-        
-    values = noise_values
+            #texture[i, j] = noise.make_fractal_sum_2D(x, y, n)
+            #texture[i, j] = noise.make_turbulence_2D(x, y, n)
+            texture[i, j] = noise.make_ridgedmf(x, y, n, 5.0)
+            #texture[i, j] = noise.make_marble_2D(x, y, n)
+       
     #normalize & show 
-    values/=values.max()
-    plt.imshow(values, cmap=plt.cm.gray)
+    texture /= texture.max()
+    plt.imshow(texture)#, cmap=plt.cm.gray)
     plt.colorbar()
     plt.show()
 
